0e7b0d269aaa584bf5b14a02949445c0ddfd434a
deezeey101 <86390876+deezeey101@users.noreply.github.com>
2021-07-08 16:25:18 +0800

    Update pythontest.py

--- pythontest.py
+++ pythontest.py
@@ -1,1 +1,13 @@
+import unittest
+from calcuupy import Calculator
 
+class CalculatorTest(unittest.TestCase):
+    calculator = Calculator()
+    def test_setupUi(self, MainWindow):
+    def test_remove_it(self):
+    def test_solver(self):
+    def test_plus_minus_it(self):
+    def test_dot_it(self):
+    def test_press_it(self, pressed):
+if __name__ == "__main__":
+    unittest.main()
